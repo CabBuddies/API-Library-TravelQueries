6094cb8bec13b1901dedf80def91e53d75f95c38
Nihal Konda <nihal.konda@sjsu.edu>
2020-04-17 01:23:49 -0700

    Opinion Create API Added

---
+++ api/opinion/index.js
@@ -0,0 +1,21 @@
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+const baseUrl = require('../../index').baseUrl;
+
+async function createOpinion(authToken,queryId,responseId,opinionType,body){
+    const data = {query:queryId,response:responseId,opinionType,body}
+    console.log('read '+JSON.stringify(data))
+    const url = baseUrl+'/comment/create'
+    return await fetch(url, {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+            'Content-Type': 'application/json; charset=utf-8',
+            'Authorization':'Basic '+authToken
+          },
+        body: JSON.stringify(data)
+      })
+}
+
+module.exports = {createOpinion}
